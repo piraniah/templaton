3f926ccfecc75864149443aba8e57c751b8611e3
Patryk Molojec <pmolojec+github@gmail.com>
2021-12-27 09:45:54 +0000

    added output files and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # templaton
+This is a simple python script to generate output.txt file based on a template.tpl and manifests.json. Script allows replacing values with env vars that can be extracted from vault or and from manifest files.
+Required files:
+* manifest.json (can be empty {})
+* template.tpl (all parameters that need to be substituted need to be prefixed with $)
--- main.py
+++ main.py
@@ -3,14 +3,19 @@ import json
 from os import environ as env
 from string import Template
 
-f_manifest = open('manifest.json')
-f_template = open('template.tpl')
+f_manifest = open('manifest.json', 'r')
+f_output = open('output.txt', 'w')
+f_template = open('template.tpl', 'r')
 manifest = json.load(f_manifest)
 template = f_template.read()
+# Merging env vars with manifest dictionary
 substitues = env | manifest
 
 result = Template(template).safe_substitute(substitues)
 print(result)
+f_output.write(result)
 
+# Closing all opened files
 f_manifest.close()
+f_output.close()
 f_template.close()
